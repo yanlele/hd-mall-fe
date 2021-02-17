a3c570a48f58ec44cbbaecd6c188215a2d4a562d
yanlele <331393627@qq.com>
2021-02-17 19:38:55 +0800

    feat: 修改model

--- packages/admin/src/pages/Product/components/ProductDetailModal/index.tsx
+++ packages/admin/src/pages/Product/components/ProductDetailModal/index.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from 'react';
 import { ProductDetailModalProps } from '@src/pages/Product/components/ProductDetailModal/interface';
-import { Modal, Image, Card, Space, Row, Col } from 'antd';
+import { Modal, Image, Card, Space, Row, Col, Divider } from 'antd';
 import styles from './style.less';
 
 const ProductDetailModal: FC<ProductDetailModalProps> = props => {
@@ -9,33 +9,48 @@ const ProductDetailModal: FC<ProductDetailModalProps> = props => {
   return (
     <Modal
       className={styles.productDetailModalContainer}
-      title="Modal 1000px width"
+      title={record.name}
       centered
       visible={visible}
       footer={null}
       onCancel={() => setVisible(false)}
       width="100%">
-      <Card style={{ width: 300, margin: '0 auto' }}>
-        <Image preview={false} width={249} src={record.primary_image} />
-        <p>title: {record.title}</p>
-        <p>desc: {record.desc}</p>
-        <Space size="middle">
-          <del>原价: {record.original_cost}</del>
-          <span>现价: {record.price}</span>
-        </Space>
-      </Card>
+      <Divider>卡片</Divider>
+      <div className="item">
+        <Card style={{ width: 300, margin: '0 auto' }}>
+          <Image preview={false} width={249} src={record.primary_image} />
+          <p>title: {record.title}</p>
+          <p>desc: {record.desc}</p>
+          <Space size="middle">
+            <del>原价: {record.original_cost}</del>
+            <span>现价: {record.price}</span>
+          </Space>
+        </Card>
+      </div>
 
-      <h3>基本信息</h3>
-      <div>
+      <Divider>基本信息</Divider>
+      <div className="item">
         <Row gutter={[16, 16]}>
-          <Col span={12}>123</Col>
-          <Col span={12}>123</Col>
+          <Col span={8}>
+            <p>id: {record.id}</p>
+          </Col>
+          <Col span={8}>
+            <p>商品类型id: {record.category_id}</p>
+          </Col>
 
-          <Col span={12}>123</Col>
-          <Col span={12}>123</Col>
+          <Col span={8}>
+            <p>库存: {record.inventory}</p>
+          </Col>
+          <Col span={8}>
+            <p>销量：{record.sales}</p>
+          </Col>
 
-          <Col span={12}>123</Col>
-          <Col span={12}>123</Col>
+          <Col span={8}>
+            <p>打标类型：{record.tag}</p>
+          </Col>
+          <Col span={8}>
+            <p>商品现在的状态： {record.status}</p>
+          </Col>
         </Row>
       </div>
     </Modal>
