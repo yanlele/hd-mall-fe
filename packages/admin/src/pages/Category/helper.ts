7c5ecd87a7c0107492cc3108efc9422c4cc9cde2
yanlele <331393627@qq.com>
2021-02-19 18:01:45 +0800

    feat: 修改数据key

--- packages/admin/src/pages/Category/helper.ts
+++ packages/admin/src/pages/Category/helper.ts
@@ -1,6 +1,7 @@
 import { HandleGetCategoryListAsyncHelperOptions } from '@src/pages/Category/interface';
 import { getCategoryListRequest } from '@src/pages/Category/service';
 import { CategoryListModel } from '@src/pages/Category/models/interface';
+import { map } from 'lodash';
 
 export const handleGetCategoryListAsyncHelper = async (
   options: HandleGetCategoryListAsyncHelperOptions<CategoryListModel>,
@@ -9,5 +10,6 @@ export const handleGetCategoryListAsyncHelper = async (
 
   setState({ loading: true });
   const res = await getCategoryListRequest();
-  setState({ list: res.data, loading: false });
+  const list = map(res.data, item => ({ ...item, key: item.id }));
+  setState({ list, loading: false });
 };
