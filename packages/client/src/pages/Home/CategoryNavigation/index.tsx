8d3627d1236703b360d1a45fbeb3865306f29844
yanlele <331393627@qq.com>
2021-02-21 15:26:21 +0800

    feat: 修改

--- packages/client/src/pages/Home/CategoryNavigation/index.tsx
+++ packages/client/src/pages/Home/CategoryNavigation/index.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from 'react';
 import { Menu } from 'antd';
+import { range, map } from 'lodash';
 import styles from './style.less';
 
 const { SubMenu } = Menu;
@@ -9,10 +10,14 @@ const CategoryNavigation: FC = () => {
     <div className={styles.categoryNavContainer}>
       {/* @ts-ignore */}
       <Menu getPopupContainer={node => node.parentElement}>
-        <SubMenu title="自动化玩具">
-          <Menu.Item>盲盒</Menu.Item>
-          <Menu.Item>机器玩具</Menu.Item>
-        </SubMenu>
+        {map(range(1, 12), item => {
+          return (
+            <SubMenu title={`自动化玩具 - ${item}`}>
+              <Menu.Item>盲盒</Menu.Item>
+              <Menu.Item>机器玩具</Menu.Item>
+            </SubMenu>
+          );
+        })}
       </Menu>
     </div>
   );
