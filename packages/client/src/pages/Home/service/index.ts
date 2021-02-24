d95c39fea1d0fd7f4e85a88d51a8da04aed59c0e
yanlele <331393627@qq.com>
2021-02-25 00:20:52 +0800

    feat: 修改接口

--- packages/client/src/pages/Home/service/index.ts
+++ packages/client/src/pages/Home/service/index.ts
@@ -1,8 +1,14 @@
-import { getCategoryListApiUrl } from '@hd/common/service/consts';
+import { getCategoryListApiUrl, getProductListApiUrl } from '@hd/common/service/consts';
 import request from '@src/utils/axios';
 
 // 获取分类列表
 export const GetCategoryListRequest = () =>
   request({
     url: getCategoryListApiUrl,
+  });
+
+export const GetProductListRequest = (params: { category_id: number }) =>
+  request({
+    url: getProductListApiUrl,
+    params,
   });
--- packages/common/service/consts.ts
+++ packages/common/service/consts.ts
@@ -2,3 +2,5 @@ import { ApiClientPrefix } from '../consts';
 
 // 获取分类列表
 export const getCategoryListApiUrl = `${ApiClientPrefix}/category/list/`
+
+export const getProductListApiUrl = `${ApiClientPrefix}/product/list`;
