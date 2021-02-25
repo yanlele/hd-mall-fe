0d43a4784eddd2001704b55ab8e2768b9e1fae45
yanlele <331393627@qq.com>
2021-02-26 00:04:23 +0800

    feat: 修改文档

--- packages/common/service/consts.ts
+++ packages/common/service/consts.ts
@@ -3,4 +3,8 @@ import { ApiClientPrefix } from '../consts';
 // 获取分类列表
 export const getCategoryListApiUrl = `${ApiClientPrefix}/category/list/`
 
+// 获取商品列表
 export const getProductListApiUrl = `${ApiClientPrefix}/product/list`;
+
+// 获取主要分类的商品
+// export const getCategoryPrimaryListApiUrl = `${ApiClientPrefix}/`
