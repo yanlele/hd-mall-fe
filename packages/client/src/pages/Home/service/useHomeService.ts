94cabf971cf4ef8e0c9309cd2bb7328d16a41d7a
yanlele <331393627@qq.com>
2021-03-07 23:32:06 +0800

    feat: 方法改造

--- packages/client/src/pages/Home/service/useHomeService.ts
+++ packages/client/src/pages/Home/service/useHomeService.ts
@@ -1,23 +1,30 @@
-import { RecoilState, useSetRecoilState, useRecoilState } from 'recoil';
+import { RecoilState, useRecoilState } from 'recoil';
 import { CategoryItem, PrimaryCategory } from '@src/pages/Home/service/interface';
-import { GetCategoryListRequest, getPrimaryCategoryList } from '@src/pages/Home/service/index';
-import { useRequest } from 'ahooks';
+import { getPrimaryCategoryList } from '@src/pages/Home/service/index';
+import { useMount } from 'ahooks';
 import { get, isEmpty } from 'lodash';
+import { getCategoryListRequest } from '../../../../../admin/src/pages/Category/service';
 
 // 请求获取 category list 数据
 export const useGetCategoryList = (categoryListModel: RecoilState<CategoryItem[]>) => {
   const [list, setCategoryList] = useRecoilState(categoryListModel);
-  if (isEmpty(list)) {
-    const { data: res } = useRequest(GetCategoryListRequest);
-    if (get(res, 'data')) setCategoryList(res.data);
-  }
+
+  useMount(() => {
+    if (isEmpty(list))
+      getCategoryListRequest().then(res => {
+        if (get(res, 'data')) setCategoryList(res.data);
+      });
+  });
 };
 
 // 获取主要分类和分类下面的商品
 export const useGetPrimaryCategoryList = (primaryCategoryModel: RecoilState<PrimaryCategory[]>) => {
   const [state, setState] = useRecoilState(primaryCategoryModel);
-  if (isEmpty(state)) {
-    const { data: res } = useRequest(getPrimaryCategoryList);
-    if (get(res, 'data')) setState(res.data);
-  }
+
+  useMount(() => {
+    if (isEmpty(state))
+      getPrimaryCategoryList().then(res => {
+        if (get(res, 'data')) setState(res.data);
+      });
+  });
 };
