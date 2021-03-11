aaf8e889ecdd6ff58b3674b0586af82644addf7c
yanlele <331393627@qq.com>
2021-03-12 00:01:03 +0800

    feat: Classify

--- packages/client/src/pages/List/ListSearchHeader/Classify/index.tsx
+++ packages/client/src/pages/List/ListSearchHeader/Classify/index.tsx
@@ -1,11 +1,34 @@
-import React, { FC } from 'react';
+import React, { FC, useMemo } from 'react';
+import { Row, Col } from 'antd';
+import { map } from 'lodash';
 import styles from './style.less';
+import { useRecoilValue } from 'recoil';
+import { primaryCategoryListModel } from '@src/pages/Home/model';
+import { Link } from 'react-router-dom';
 
 const Classify: FC = () => {
+  const list = useRecoilValue(primaryCategoryListModel);
+  console.log('list', list);
+
+  const handleRenderClassifyItem = useMemo(() => {
+    return map(list, item => {
+      return (
+        <p className="item">
+          <Link to="list">{item.name}</Link>
+        </p>
+      );
+    });
+  }, [list]);
+
   return (
-    <div className={styles.classifyContainer}>
-      <span>分类 Classify</span>
-    </div>
+    <Row className={styles.classifyContainer}>
+      <Col span={2}>
+        <p className="label">分类：</p>
+      </Col>
+      <Col className="content" span={22}>
+        {handleRenderClassifyItem}
+      </Col>
+    </Row>
   );
 };
 
