3fad17fff4bfefa7507319267ebb6275db388160
yanlele <331393627@qq.com>
2021-03-14 23:59:29 +0800

    feat: 修改footer

--- packages/client/src/components/biz/FooterComponent/index.tsx
+++ packages/client/src/components/biz/FooterComponent/index.tsx
@@ -5,7 +5,24 @@ import styles from './style.less';
 const { Footer } = Layout;
 
 const FooterComponent: FC = () => {
-  return <Footer className={styles.footerContainer}>footer</Footer>;
+  // 图片url https://gitee.com/yanle-static/static/raw/master/hd-mall/1615735552006-footer-img1.png
+  return (
+    <Footer className={styles.footerContainer}>
+      <div className="content">
+        <img
+          className="footer-image1"
+          src="https://gitee.com/yanle-static/static/raw/master/hd-mall/1615735552006-footer-img1.png"
+          alt="1"
+        />
+
+        <div className="content-right">
+          <p>周一至周日 8:00 - 23:00</p>
+          <p>240-3456-8888</p>
+          <p>在线客服</p>
+        </div>
+      </div>
+    </Footer>
+  );
 };
 
 export default FooterComponent;
