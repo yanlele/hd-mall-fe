15d2fa823456ecc7e5d0b9aaa71765dd8484c45d
yanlele <331393627@qq.com>
2021-03-14 22:28:04 +0800

    feat: 添加类型

--- packages/client/src/pages/List/SortType/index.tsx
+++ packages/client/src/pages/List/SortType/index.tsx
@@ -1,10 +1,19 @@
 import React, { FC } from 'react';
 import styles from './style.less';
+import { Divider } from 'antd';
 
 const SortType: FC = () => {
   return (
     <div className={styles.sortTypeContainer}>
-      <p>12313123kjkljlks dfslk flskjf lsd flsdj fl sdlfj lsd j</p>
+      <a className="item">Link</a>
+      <Divider type="vertical" />
+      <a className="item">Link</a>
+      <Divider type="vertical" />
+      <a className="item">Link</a>
+      <Divider type="vertical" />
+      <a className="item">Link</a>
+      <Divider type="vertical" />
+      <a className="item">Link</a>
     </div>
   );
 };
