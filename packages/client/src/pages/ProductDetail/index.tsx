1e93c58b8410df0edf6837c35f096e98b6e00ba2
yanlele <331393627@qq.com>
2021-03-26 23:24:53 +0800

    feat: 添加content

--- packages/client/src/pages/ProductDetail/index.tsx
+++ packages/client/src/pages/ProductDetail/index.tsx
@@ -1,7 +1,21 @@
 import React, { FC } from 'react';
+import HomeHeader from '@src/components/biz/HomeHeader';
+import { useGetPrimaryCategoryList } from '@src/pages/Home/service/useHomeService';
+import { primaryCategoryListModel } from '@src/pages/Home/model';
+import styles from './style.less';
 
 const ProductDetail: FC = () => {
-  return <div>1231231 123123 123 123 12 3 ProductDetail</div>;
+  // 获取所有分类
+  // 获取主要分类和产品
+  useGetPrimaryCategoryList(primaryCategoryListModel);
+
+  return (
+    <div className={styles.productDetailContainer}>
+      <HomeHeader />
+
+      <div className="detail-content">hello</div>
+    </div>
+  );
 };
 
 export default ProductDetail;
