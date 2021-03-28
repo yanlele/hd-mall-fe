3bf5bb3421f2061dbfc114bfca78cae2942bd975
yanlele <331393627@qq.com>
2021-03-28 21:46:15 +0800

    feat: 添加备注

--- packages/client/src/pages/ProductDetail/index.tsx
+++ packages/client/src/pages/ProductDetail/index.tsx
@@ -4,6 +4,10 @@ import { useGetPrimaryCategoryList } from '@src/pages/Home/service/useHomeServic
 import { primaryCategoryListModel } from '@src/pages/Home/model';
 import styles from './style.less';
 
+/**
+ * 详情页面
+ * @constructor
+ */
 const ProductDetail: FC = () => {
   // 获取所有分类
   // 获取主要分类和产品
