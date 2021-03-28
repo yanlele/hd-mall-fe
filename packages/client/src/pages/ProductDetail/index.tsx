7f370f1e5a6318a3e0041b6434cc250a4da69176
yanlele <331393627@qq.com>
2021-03-29 00:25:57 +0800

    feat: 侧边栏画册完成

--- packages/client/src/pages/ProductDetail/index.tsx
+++ packages/client/src/pages/ProductDetail/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import HomeHeader from '@src/components/biz/HomeHeader';
 import { useGetPrimaryCategoryList } from '@src/pages/Home/service/useHomeService';
 import { primaryCategoryListModel } from '@src/pages/Home/model';
@@ -13,7 +13,7 @@ SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
 
 const list = [
   'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fattach.bbs.miui.com%2Fforum%2F201303%2F18%2F233101zl8ouwl8l8e8eu7e.jpg&refer=http%3A%2F%2Fattach.bbs.miui.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1619537403&t=89b82ab1d4c58ee8c656c4d1536fcaef',
-  'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fattach.bbs.miui.com%2Fforum%2F201307%2F24%2F213459izmmcsheybh3z6ee.jpg&refer=http%3A%2F%2Fattach.bbs.miui.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1619537404&t=1b8a18fd772414a19d334a4104e437e1',
+  'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3726272358,2619832442&fm=26&gp=0.jpg',
   'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fattach.bbs.miui.com%2Fforum%2F201304%2F11%2F130724xpp1t8cduuq8d1u4.jpg&refer=http%3A%2F%2Fattach.bbs.miui.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1619537405&t=b2e69b3d1433b5cffbd9c3b6ea652d67',
   'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fattach.bbs.miui.com%2Fforum%2F201305%2F29%2F101838y2tkuxpfopkwzpvg.jpg&refer=http%3A%2F%2Fattach.bbs.miui.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1619537405&t=50d3fcf9889e687aa57d2d6fcd3499b7',
   'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fattach.bbs.miui.com%2Fforum%2F201205%2F15%2F152008d7vjov6j6zdo6o8z.jpg&refer=http%3A%2F%2Fattach.bbs.miui.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1619537406&t=4ae6731dd9ea0ea54e7b0d5968e1cf17',
@@ -35,6 +35,8 @@ const ProductDetail: FC = () => {
   // 获取主要分类和产品
   useGetPrimaryCategoryList(primaryCategoryListModel);
 
+  const [mainImage, setMainImage] = useState(list[0]);
+
   return (
     <div className={styles.productDetailContainer}>
       <HomeHeader />
@@ -43,10 +45,7 @@ const ProductDetail: FC = () => {
         <div className="main-content">
           <div className="image-container">
             <div className="main-img">
-              <img
-                src="https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fattach.bbs.miui.com%2Fforum%2F201311%2F01%2F215828tpmddz2d2bfcz5pk.jpg&refer=http%3A%2F%2Fattach.bbs.miui.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1619537124&t=cdd81ccb8c0c30142f65cc25e5f3296d"
-                alt=""
-              />
+              <img src={mainImage} alt="" />
             </div>
             <div className="img-list">
               <div className="image-prev">
@@ -65,7 +64,7 @@ const ProductDetail: FC = () => {
                 {map(list, (item, index) => {
                   return (
                     <SwiperSlide key={index} className="swiper-slide">
-                      <img width={70} src={item} alt="" />
+                      <img onClick={() => setMainImage(item)} width={70} src={item} alt="" />
                     </SwiperSlide>
                   );
                 })}
