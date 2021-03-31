a054aba6a4159f27b217f7e6b87ea9273035f454
yanlele <331393627@qq.com>
2021-03-31 22:45:40 +0800

    feat: 添加样式

--- packages/client/src/pages/ProductDetail/components/ProductInfo/index.tsx
+++ packages/client/src/pages/ProductDetail/components/ProductInfo/index.tsx
@@ -4,7 +4,8 @@ import styles from './style.less';
 const ProductInfo: FC = () => {
   return (
     <div className={styles.productInfoContainer}>
-      <div className="detail-container">detail</div>
+      <h2 className="title">乐高什么仙女玩具外级逆回购傻逼大傻子</h2>
+      <p className="desc-info">品质好物 优先选购</p>
     </div>
   );
 };
