d464fe16c3bdccfa677832f1c1b4a825e8277815
yanlele <331393627@qq.com>
2021-03-31 23:40:15 +0800

    feat: 修改样式

--- packages/client/src/pages/ProductDetail/components/ProductInfo/index.tsx
+++ packages/client/src/pages/ProductDetail/components/ProductInfo/index.tsx
@@ -6,6 +6,32 @@ const ProductInfo: FC = () => {
     <div className={styles.productInfoContainer}>
       <h2 className="title">乐高什么仙女玩具外级逆回购傻逼大傻子</h2>
       <p className="desc-info">品质好物 优先选购</p>
+
+      <div className="price-content">
+        <p className="line">
+          <span className="label">原价</span>
+          <span className="price">
+            <del>￥880.00</del>
+          </span>
+        </p>
+
+        <p className="line">
+          <span className="label">价格</span>
+          <span className="price">￥580.00</span>
+        </p>
+
+        <p className="line">
+          <span className="label">促销</span>
+          <span className="discount-icon">赠送积分</span>
+          <span className="desc">购买即赠送商城积分，积分可以抵现</span>
+        </p>
+      </div>
+
+      <p className="distribution">
+        <span>配送</span>
+        <span>快递满99免邮费</span>
+        <span>付款后72小时内发货</span>
+      </p>
     </div>
   );
 };
