160d8ba56e8ba2c44045ad26e9b0c393129e2361
yanlele <331393627@qq.com>
2021-03-31 22:37:13 +0800

    feat: 抽象 main image container

---
+++ packages/client/src/pages/ProductDetail/components/ProductInfo/index.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react';
+
+const ProductInfo: FC = () => {
+  return (
+    <div>
+      <div className="detail-container">detail</div>
+    </div>
+  );
+};
+
+export default ProductInfo;
