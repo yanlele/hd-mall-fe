f7f9ed89fd073aa4fd0ce72801051e7d8602a8d9
yanlele <331393627@qq.com>
2021-04-04 17:46:51 +0800

    feat: 图片容器

--- packages/client/src/pages/ProductDetail/components/DetailImage/index.tsx
+++ packages/client/src/pages/ProductDetail/components/DetailImage/index.tsx
@@ -32,6 +32,17 @@ const DetailImage: FC = () => {
           </Row>
         </div>
       </div>
+
+      <div className="detail-image-list">
+        <img src="https://p3.ssl.qhmsg.com/t01a66bf5c423d2fbc2.webp" alt="" />
+        <img src="https://p1.ssl.qhmsg.com/t01d5aca6061c7fbf4d.webp" alt="" />
+        <img src="https://p3.ssl.qhmsg.com/t01fd9a45f246b713a7.webp" alt="" />
+        <img src="https://p4.ssl.qhmsg.com/t018e59b475a5a0da68.webp" alt="" />
+        <img src="https://p0.ssl.qhmsg.com/t0101607f085693bb7e.webp" alt="" />
+        <img src="https://p4.ssl.qhmsg.com/t018a74bd727f3c79ad.webp" alt="" />
+        <img src="https://p2.ssl.qhmsg.com/t01c09beaefccf252f1.webp" alt="" />
+        <img src="https://p3.ssl.qhmsg.com/t01202739971b60b565.webp" alt="" />
+      </div>
     </div>
   );
 };
