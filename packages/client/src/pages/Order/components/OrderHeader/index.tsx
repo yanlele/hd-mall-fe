e4dda19ba2bc77aad66177af7dba1319e50c2ec7
yanlele <331393627@qq.com>
2021-04-04 23:32:33 +0800

    feat: order header

--- packages/client/src/pages/Order/components/OrderHeader/index.tsx
+++ packages/client/src/pages/Order/components/OrderHeader/index.tsx
@@ -1,10 +1,23 @@
 import React, { FC } from 'react';
 import styles from './style.less';
+import { Steps } from 'antd';
 
+const { Step } = Steps;
+
+/**
+ * order header
+ * @constructor
+ */
 const OrderHeader: FC = () => {
   return (
     <div className={styles.orderHeaderContainer}>
-      <div className="header-content">OrderHeader</div>
+      <div className="header-content">
+        <h2>确认订单</h2>
+        <Steps className="steps" progressDot current={0}>
+          <Step title="填写核对订单" />
+          <Step title="成功提交订单" />
+        </Steps>
+      </div>
     </div>
   );
 };
