44e96d183924d881d43ef6f3eae3cd3b5bf5a01f
yanlele <331393627@qq.com>
2021-04-05 23:28:07 +0800

    feat: 修改

--- packages/client/src/pages/Order/components/OrderAddress/index.tsx
+++ packages/client/src/pages/Order/components/OrderAddress/index.tsx
@@ -6,6 +6,7 @@ const OrderAddress: FC = () => {
     <div className={styles.orderAddressContainer}>
       <h3>收获地址</h3>
       <div className="address-list">
+        {/* address - item */}
         <div className="address-item">
           <p className="info">
             <span className="name">胡大胖</span>
@@ -22,6 +23,11 @@ const OrderAddress: FC = () => {
           <p className="address-desc">
             说到缴罚了开始就拉开激动啊偶尔不见阿尔里看不见拉开东方既白奥地利飞机杯拉肯德基封闭欧文人
           </p>
+        </div>
+
+        {/* address - add */}
+        <div className="address-add">
+          <p className="add-text">add-text</p>
         </div>
       </div>
     </div>
