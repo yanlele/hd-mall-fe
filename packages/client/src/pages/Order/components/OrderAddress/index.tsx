81df8a0597a6998ca41326e20ef9b78e379be707
yanlele <331393627@qq.com>
2021-04-05 23:42:46 +0800

    feat: 修改

--- packages/client/src/pages/Order/components/OrderAddress/index.tsx
+++ packages/client/src/pages/Order/components/OrderAddress/index.tsx
@@ -1,5 +1,7 @@
 import React, { FC } from 'react';
+import { range, map } from 'lodash';
 import styles from './style.less';
+import { PlusOutlined } from '@ant-design/icons';
 
 const OrderAddress: FC = () => {
   return (
@@ -7,27 +9,36 @@ const OrderAddress: FC = () => {
       <h3>收获地址</h3>
       <div className="address-list">
         {/* address - item */}
-        <div className="address-item">
-          <p className="info">
-            <span className="name">胡大胖</span>
-            <span className="handle">
-              <a
-                onClick={() => {
-                  console.log('123123');
-                }}>
-                修改
-              </a>
-            </span>
-          </p>
-          <p className="phone">15213498872</p>
-          <p className="address-desc">
-            说到缴罚了开始就拉开激动啊偶尔不见阿尔里看不见拉开东方既白奥地利飞机杯拉肯德基封闭欧文人
-          </p>
-        </div>
+        {map(range(3), (item, index) => {
+          return (
+            <div key={index} className="address-item">
+              <p className="info">
+                <span className="name">胡大胖 - {item}</span>
+                <span className="handle">
+                  <a
+                    onClick={() => {
+                      console.log('123123');
+                    }}>
+                    修改
+                  </a>
+                </span>
+              </p>
+              <p className="phone">15213498872</p>
+              <p className="address-desc">
+                说到缴罚了开始就拉开激动啊偶尔不见阿尔里看不见拉开东方既白奥地利飞机杯拉肯德基封闭欧文人
+              </p>
+            </div>
+          );
+        })}
 
         {/* address - add */}
         <div className="address-add">
-          <p className="add-text">add-text</p>
+          <p className="add-text">
+            <span className="add-icon">
+              <PlusOutlined />
+            </span>
+            <span className="content">新增收货地址</span>
+          </p>
         </div>
       </div>
     </div>
