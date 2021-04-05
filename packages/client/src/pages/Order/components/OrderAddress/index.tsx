79102733195272ab6fae3c47f0f9a03424a8eb26
yanlele <331393627@qq.com>
2021-04-05 23:07:08 +0800

    feat: order item

--- packages/client/src/pages/Order/components/OrderAddress/index.tsx
+++ packages/client/src/pages/Order/components/OrderAddress/index.tsx
@@ -7,12 +7,21 @@ const OrderAddress: FC = () => {
       <h3>收获地址</h3>
       <div className="address-list">
         <div className="address-item">
-          <p>
-            <span>胡大胖</span>
-            <span>修改</span>
+          <p className="info">
+            <span className="name">胡大胖</span>
+            <span className="handle">
+              <a
+                onClick={() => {
+                  console.log('123123');
+                }}>
+                修改
+              </a>
+            </span>
           </p>
-          <p>15213498872</p>
-          <p>重庆市南岸区南山 重庆第二师范学院</p>
+          <p className="phone">15213498872</p>
+          <p className="address-desc">
+            说到缴罚了开始就拉开激动啊偶尔不见阿尔里看不见拉开东方既白奥地利飞机杯拉肯德基封闭欧文人
+          </p>
         </div>
       </div>
     </div>
