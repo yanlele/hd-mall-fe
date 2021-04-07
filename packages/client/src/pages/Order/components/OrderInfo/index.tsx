68578c57e06334df9a07d025523894f91a76a859
yanlele <331393627@qq.com>
2021-04-07 23:52:50 +0800

    feat: 数量调整

--- packages/client/src/pages/Order/components/OrderInfo/index.tsx
+++ packages/client/src/pages/Order/components/OrderInfo/index.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from 'react';
 import styles from './style.less';
 import { Button, Input } from 'antd';
+import { range, map } from 'lodash';
 
 const { TextArea } = Input;
 
@@ -13,22 +14,27 @@ const OrderInfo: FC = () => {
 
       <div className="order-info-content">
         {/* 商品信息 */}
-        <div className="order-info-item">
-          <div className="info-content">
-            <img
-              className="order-info-image"
-              src="https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=3101694723,748884042&fm=26&gp=0.jpg"
-              alt=""
-            />
-            <span className="content-desc">
-              乐高哈利沙特森美城堡魔法世界男女拼积木益智玩具乐高哈利沙特森美城堡魔法世界男女拼积木益智玩具
-            </span>
-          </div>
-          <div className="price-content">
-            <span className="count">X 1</span>
-            <span className="price">$ 2889.00</span>
-          </div>
-        </div>
+        {map(range(2), item => {
+          return (
+            <div key={item} className="order-info-item">
+              <div className="info-content">
+                <img
+                  className="order-info-image"
+                  src="https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=3101694723,748884042&fm=26&gp=0.jpg"
+                  alt=""
+                />
+                <span className="content-desc">
+                  乐高哈利沙特森美城堡魔法世界男女拼积木益智玩具乐高哈利沙特森美城堡魔法世界男女拼积木益智玩具
+                </span>
+              </div>
+              <div className="price-content">
+                <span className="count">X 1</span>
+                <span className="price">$ 2889.00</span>
+              </div>
+            </div>
+          );
+        })}
+
         {/* 配送方式 */}
         <div className="distribution">
           <span className="label">配送方式：</span>
