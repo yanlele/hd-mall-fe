3b1515991a87a861be8337ea243f77eb1c670300
yanlele <331393627@qq.com>
2021-04-07 23:47:43 +0800

    feat: 订单基本切图完成

--- packages/client/src/pages/Order/components/OrderInfo/index.tsx
+++ packages/client/src/pages/Order/components/OrderInfo/index.tsx
@@ -1,5 +1,8 @@
 import React, { FC } from 'react';
 import styles from './style.less';
+import { Button, Input } from 'antd';
+
+const { TextArea } = Input;
 
 const OrderInfo: FC = () => {
   return (
@@ -9,6 +12,7 @@ const OrderInfo: FC = () => {
       </div>
 
       <div className="order-info-content">
+        {/* 商品信息 */}
         <div className="order-info-item">
           <div className="info-content">
             <img
@@ -25,6 +29,49 @@ const OrderInfo: FC = () => {
             <span className="price">$ 2889.00</span>
           </div>
         </div>
+        {/* 配送方式 */}
+        <div className="distribution">
+          <span className="label">配送方式：</span>
+          <span className="value">普通配送 快递包邮</span>
+        </div>
+        {/* 发票 */}
+        <div className="invoice">
+          <span className="label">发票信息：</span>
+          <div className="value content">
+            <p className="line-one">注：如果商品由第三方卖家销售，发票内容由其卖家决定，发票由卖家开具并寄出</p>
+            <p>电子普通发票 个人</p>
+          </div>
+        </div>
+        {/* 留言 */}
+        <div className="remark">
+          <span className="label">留言：</span>
+          <TextArea style={{ width: '60%' }} rows={6} autoSize={{ maxRows: 6, minRows: 6 }} placeholder="给商家留言" />
+        </div>
+      </div>
+
+      {/* 支付信息 */}
+      <div className="pay-info">
+        <p className="text">
+          <span className="label">商品总金额：</span>
+          <span className="value">￥2899.00</span>
+        </p>
+
+        <p className="text">
+          <span className="label">运费：</span>
+          <span className="value">￥0</span>
+        </p>
+
+        <p className="text">
+          <span className="label">优惠金额：</span>
+          <span className="value">￥0</span>
+        </p>
+
+        <p className="text">
+          <span className="label">应付金额：</span>
+          <span className="value price">￥2899.00</span>
+        </p>
+
+        <Button className="confirm-order-button">立即下单</Button>
       </div>
     </div>
   );
