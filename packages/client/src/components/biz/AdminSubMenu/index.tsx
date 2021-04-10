6a48003b26ec73e5b4709688f520380ce1c80f2c
yanlele <331393627@qq.com>
2021-04-10 22:23:05 +0800

    feat: 购物车完成

--- packages/client/src/components/biz/AdminSubMenu/index.tsx
+++ packages/client/src/components/biz/AdminSubMenu/index.tsx
@@ -1,5 +1,5 @@
 import React, { FC, useMemo } from 'react';
-import { Menu } from 'antd';
+import { Menu, Affix } from 'antd';
 import { AppstoreOutlined, MailOutlined, SettingOutlined } from '@ant-design/icons';
 import { useTitle } from 'ahooks';
 import { find, get } from 'lodash';
@@ -21,29 +21,31 @@ const AdminSubMenu: FC = () => {
   }, [window.location.pathname]);
 
   return (
-    <Menu mode="inline" defaultSelectedKeys={defaultSelectedKeys} style={{ width: 168 }}>
-      <Menu.Item onClick={onOpenChange} key="/admin-my" icon={<MailOutlined />}>
-        个人中心
-      </Menu.Item>
-      <Menu.Item onClick={onOpenChange} key="/admin-shopping-cart" icon={<AppstoreOutlined />}>
-        我的购物车
-      </Menu.Item>
-      <Menu.Item onClick={onOpenChange} key="sub4" icon={<SettingOutlined />}>
-        订单中心
-      </Menu.Item>
-      <Menu.Item onClick={onOpenChange} key="sub5" icon={<SettingOutlined />}>
-        收藏
-      </Menu.Item>
-      <Menu.Item onClick={onOpenChange} key="sub6" icon={<SettingOutlined />}>
-        地址管理
-      </Menu.Item>
-      <Menu.Item onClick={onOpenChange} key="sub7" icon={<SettingOutlined />}>
-        商品评价
-      </Menu.Item>
-      <Menu.Item onClick={onOpenChange} key="sub8" icon={<SettingOutlined />}>
-        个人信息
-      </Menu.Item>
-    </Menu>
+    <Affix>
+      <Menu mode="inline" defaultSelectedKeys={defaultSelectedKeys} style={{ width: 168 }}>
+        <Menu.Item onClick={onOpenChange} key="/admin-my" icon={<MailOutlined />}>
+          个人中心
+        </Menu.Item>
+        <Menu.Item onClick={onOpenChange} key="/admin-shopping-cart" icon={<AppstoreOutlined />}>
+          我的购物车
+        </Menu.Item>
+        <Menu.Item onClick={onOpenChange} key="sub4" icon={<SettingOutlined />}>
+          订单中心
+        </Menu.Item>
+        <Menu.Item onClick={onOpenChange} key="sub5" icon={<SettingOutlined />}>
+          收藏
+        </Menu.Item>
+        <Menu.Item onClick={onOpenChange} key="sub6" icon={<SettingOutlined />}>
+          地址管理
+        </Menu.Item>
+        <Menu.Item onClick={onOpenChange} key="sub7" icon={<SettingOutlined />}>
+          商品评价
+        </Menu.Item>
+        <Menu.Item onClick={onOpenChange} key="sub8" icon={<SettingOutlined />}>
+          个人信息
+        </Menu.Item>
+      </Menu>
+    </Affix>
   );
 };
 
--- packages/client/src/pages/ShoppingCart/index.tsx
+++ packages/client/src/pages/ShoppingCart/index.tsx
@@ -1,10 +1,64 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import AdminContainer from '@src/components/biz/AdminContainer';
+import styles from './style.less';
+import { columns } from '@src/pages/ShoppingCart/helper';
+import { Affix, Button, Table } from 'antd';
+
+const data: any[] = [];
+for (let i = 0; i < 46; i++) {
+  data.push({
+    key: i,
+    name: `Edward King ${i}`,
+    age: 32,
+    address: `London, Park Lane no. ${i}`,
+  });
+}
 
 const ShoppingCart: FC = () => {
+  const [selectedRowKeys, setSelectedRowKeys] = useState<any[]>([]);
+
+  const onSelectChange = (selectedRowKeys: any[]) => {
+    console.log('selectedRowKeys changed: ', selectedRowKeys);
+    setSelectedRowKeys(selectedRowKeys);
+  };
+
+  const rowSelection = {
+    selectedRowKeys,
+    onChange: onSelectChange,
+  };
+
   return (
     <AdminContainer>
-      <div>ShoppingCart</div>
+      <div className={styles.shoppingCartContainer}>
+        <div className="content">
+          <Table
+            pagination={{
+              current: 1,
+              pageSize: 20,
+              total: 46,
+              showTitle: true,
+            }}
+            rowSelection={rowSelection}
+            columns={columns}
+            dataSource={data}
+          />
+        </div>
+      </div>
+
+      <Affix offsetBottom={12}>
+        <footer className={styles.settlement}>
+          <div>{selectedRowKeys.length > 0 && <a>删除全部</a>}</div>
+          <div className="handle-settlement">
+            <p>
+              已选择商品 <span className="count">{selectedRowKeys.length}</span> 件
+            </p>
+            <p>
+              合计：<span className="price">0.00</span>
+            </p>
+            <Button>结算</Button>
+          </div>
+        </footer>
+      </Affix>
     </AdminContainer>
   );
 };
