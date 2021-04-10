622a44476dc594ece187f1e2d5fb583b979e0118
yanlele <331393627@qq.com>
2021-04-10 17:44:25 +0800

    feat: 个人中心侧边栏

--- packages/client/src/components/biz/AdminSubMenu/index.tsx
+++ packages/client/src/components/biz/AdminSubMenu/index.tsx
@@ -1,18 +1,28 @@
-import React, { FC } from 'react';
+import React, { FC, useMemo } from 'react';
 import { Menu } from 'antd';
 import { AppstoreOutlined, MailOutlined, SettingOutlined } from '@ant-design/icons';
 import { useTitle } from 'ahooks';
+import { find, get } from 'lodash';
+import staticRotes from '@src/routers/pageRouteConfig';
+import { useHistory } from 'react-router';
 
 const AdminSubMenu: FC = () => {
   useTitle('个人中心');
+  const history = useHistory();
 
-  const onOpenChange = (keys: any) => {
-    console.log(keys);
+  const onOpenChange = (value: any) => {
+    history.push(`/${get(value, 'key')}`);
   };
 
+  const defaultSelectedKeys = useMemo(() => {
+    const pathname = window.location.pathname;
+    const currentStateRoute = find(staticRotes, item => pathname === item.path);
+    return [get(currentStateRoute, 'path') as string];
+  }, [window.location.pathname]);
+
   return (
-    <Menu mode="inline" style={{ width: 256 }}>
-      <Menu.Item onClick={onOpenChange} key="sub1" icon={<MailOutlined />}>
+    <Menu mode="inline" defaultSelectedKeys={defaultSelectedKeys} style={{ width: 256 }}>
+      <Menu.Item onClick={onOpenChange} key="/admin-my" icon={<MailOutlined />}>
         个人中心
       </Menu.Item>
       <Menu.Item onClick={onOpenChange} key="sub2" icon={<AppstoreOutlined />}>
