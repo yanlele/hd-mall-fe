dc2fa30939fcabcdc995e1859dc7ad27e324386c
yanlele <331393627@qq.com>
2021-04-18 22:50:34 +0800

    feat: 引入表单

--- packages/client/src/pages/AdminAddress/index.tsx
+++ packages/client/src/pages/AdminAddress/index.tsx
@@ -1,11 +1,20 @@
 import React, { FC } from 'react';
 import AdminContainer from '@src/components/biz/AdminContainer';
 import AdminTitleBar from '@src/components/biz/AdminTitleBar';
+import AddressForm from '@src/components/biz/AddressModal/AddressForm';
+import { clientAddressModalModel } from '@src/components/biz/AddressModal/model';
+import styles from './style.less';
 
 const AdminAddress: FC = () => {
   return (
     <AdminContainer>
       <AdminTitleBar>地址管理中心</AdminTitleBar>
+
+      <div className={styles.adminAddressContainer}>
+        <div className="form-content">
+          <AddressForm model={clientAddressModalModel} />
+        </div>
+      </div>
     </AdminContainer>
   );
 };
