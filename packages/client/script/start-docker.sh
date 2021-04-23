258010eec83bf6397f86d284eb91dfa4cd555a6f
yanlele <331393627@qq.com>
2021-04-23 21:14:17 +0800

    feat: 修改sh文件

--- packages/client/script/start-docker.sh
+++ packages/client/script/start-docker.sh
@@ -1,1 +1,5 @@
-docker run --restart=always  -it -d --name client-admin -p 8000:80 -v /root/app/hd-mall-fe/web-client/default.conf:/etc/nginx/conf.d/default.conf -v /root/app/hd-mall-fe/web-client/nginx.conf:/etc/nginx/conf.conf -v /root/app/hd-mall-fe/web-client/dist:/usr/share/nginx/html -v /root/app/hd-mall-fe/web-client/logs:/var/log/nginx nginx
+docker run --restart=always  -it -d --name client-admin -p 8000:80 \
+ -v /root/app/hd-mall-fe/web-client/default.conf:/etc/nginx/conf.d/default.conf \
+ -v /root/app/hd-mall-fe/web-client/nginx.conf:/etc/nginx/conf.conf \
+ -v /root/app/hd-mall-fe/web-client/dist:/usr/share/nginx/html \
+ -v /root/app/hd-mall-fe/web-client/logs:/var/log/nginx nginx
