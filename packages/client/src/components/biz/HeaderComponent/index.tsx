10ff458fe9131d78e787e219ce6f656dceaee324
yanlele <331393627@qq.com>
2021-05-01 15:41:59 +0800

    feat: 登录

--- packages/client/src/components/biz/HeaderComponent/index.tsx
+++ packages/client/src/components/biz/HeaderComponent/index.tsx
@@ -1,31 +1,75 @@
-import React, { FC } from 'react';
+import React, { FC, useMemo } from 'react';
 import { Layout, Divider } from 'antd';
 import styles from './style.less';
 import { Link } from 'react-router-dom';
+import { useRecoilState } from 'recoil';
+import { userInfoModel } from '@src/components/biz/UserLoginComponent/model';
+import { usePersistFn } from 'ahooks';
+import { produce } from 'immer';
+import UserLoginComponent from '@src/components/biz/UserLoginComponent';
 
 const { Header } = Layout;
 
 const HeaderComponent: FC = () => {
-  return (
-    <Header className={styles.header}>
-      <div className="header-content">
-        <div className="left">
-          <span>在线商城</span>
-          <Divider type="vertical" />
-          <Link to="/admin/order-list">我的订单</Link>
-        </div>
+  const [{ userInfo }, setUserState] = useRecoilState(userInfoModel);
+  const { user_id } = userInfo;
 
-        <div className="right">
-          <a>注册</a>
+  const handleLogin = usePersistFn(() => {
+    setUserState(
+      produce(draft => {
+        draft.modalControl.visible = true;
+        draft.modalControl.type = 'login';
+      }),
+    );
+  });
+
+  const handleRegister = usePersistFn(() => {
+    setUserState(
+      produce(draft => {
+        draft.modalControl.visible = true;
+        draft.modalControl.type = 'register';
+      }),
+    );
+  });
+
+  const handleRenderUser = useMemo(() => {
+    if (user_id) {
+      return (
+        <>
+          <a>退出登录</a>
           <Divider type="vertical" />
-          <a>登录</a>
-          <Divider type="vertical" />
-          <a>消息通知</a>
-          <Divider type="vertical" />
-          <Link to="/admin/shopping-cart">购物车</Link>
+        </>
+      );
+    }
+    return (
+      <>
+        <a onClick={handleRegister}>注册</a>
+        <Divider type="vertical" />
+        <a onClick={handleLogin}>登录</a>
+        <Divider type="vertical" />
+      </>
+    );
+  }, [user_id]);
+
+  return (
+    <>
+      <Header className={styles.header}>
+        <div className="header-content">
+          <div className="left">
+            <span>在线商城</span>
+            <Divider type="vertical" />
+            <Link to="/admin/order-list">我的订单</Link>
+          </div>
+
+          <div className="right">
+            {handleRenderUser}
+            <Link to="/admin/shopping-cart">购物车</Link>
+          </div>
         </div>
-      </div>
-    </Header>
+      </Header>
+
+      <UserLoginComponent />
+    </>
   );
 };
 
--- packages/client/src/components/biz/UserLoginComponent/index.tsx
+++ packages/client/src/components/biz/UserLoginComponent/index.tsx
@@ -1,40 +1,56 @@
-import React, { FC } from 'react';
-import { Modal, Form, Input } from 'antd';
+import React, { FC, useMemo } from 'react';
+import { Modal, Form, Input, message } from 'antd';
+import { useRecoilState } from 'recoil';
+import { userInfoModel } from '@src/components/biz/UserLoginComponent/model';
+import { usePersistFn } from 'ahooks';
+import { produce } from 'immer';
+import { defaultUserInfoModelState } from '@src/components/biz/UserLoginComponent/model/consts';
 
 const layout = {
-  labelCol: { span: 8 },
+  labelCol: { span: 6 },
   wrapperCol: { span: 16 },
 };
 
 const UserLoginComponent: FC = () => {
+  const [{ modalControl }, setState] = useRecoilState(userInfoModel);
+  const { visible, type } = modalControl;
   const [form] = Form.useForm();
+
+  const useTitle = useMemo(() => (type === 'login' ? '登录' : '注册'), [type]);
+
+  const handleOnSubmit = usePersistFn(() => {
+    const { validateFields } = form;
+    validateFields().then(res => {
+      console.log(res);
+      message.success(`${useTitle}成功`);
+      handleOnChancel();
+    });
+  });
+
+  const handleOnChancel = usePersistFn(() => {
+    setState(
+      produce(draft => {
+        draft.modalControl = defaultUserInfoModelState.modalControl;
+      }),
+    );
+  });
 
   return (
     <Modal
       destroyOnClose
-      title={'登录'}
+      title={useTitle}
       centered
-      visible={false}
-      onOk={() => {
-        console.log('ok');
-      }}
+      visible={visible}
+      onOk={handleOnSubmit}
       width={'640px'}
       confirmLoading={false}
-      onCancel={() => {
-        console.log('cancel');
-      }}>
+      onCancel={handleOnChancel}>
       <Form form={form} {...layout} name="basic" initialValues={{ remember: true }}>
-        <Form.Item
-          label="Username"
-          name="username"
-          rules={[{ required: true, message: 'Please input your username!' }]}>
+        <Form.Item label="用户名" name="username" rules={[{ required: true, message: 'Please input your username!' }]}>
           <Input />
         </Form.Item>
 
-        <Form.Item
-          label="Password"
-          name="password"
-          rules={[{ required: true, message: 'Please input your password!' }]}>
+        <Form.Item label="Password" name="密码" rules={[{ required: true, message: 'Please input your password!' }]}>
           <Input.Password />
         </Form.Item>
       </Form>
