eb8c88b5e50ab266c397e13c4e9dba97f79a87cb
yanlele <331393627@qq.com>
2021-05-02 16:08:59 +0800

    feat: 修改样式引用

--- packages/client/src/pages/List/SortType/index.tsx
+++ packages/client/src/pages/List/SortType/index.tsx
@@ -5,15 +5,13 @@ import { Divider } from 'antd';
 const SortType: FC = () => {
   return (
     <div className={styles.sortTypeContainer}>
-      <a className="item">Link</a>
+      <a className="item">综合</a>
       <Divider type="vertical" />
-      <a className="item">Link</a>
+      <a className="item">销量</a>
       <Divider type="vertical" />
-      <a className="item">Link</a>
+      <a className="item">新品</a>
       <Divider type="vertical" />
-      <a className="item">Link</a>
-      <Divider type="vertical" />
-      <a className="item">Link</a>
+      <span className="item">价格区间</span>
     </div>
   );
 };
