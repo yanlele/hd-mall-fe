a0ac2ed17881f6a7d30998cef2c41a189ba3b7c8
yanlele <331393627@qq.com>
2021-05-02 17:25:49 +0800

    feat: 列表场景齐全

--- packages/client/src/pages/List/service/interface.ts
+++ packages/client/src/pages/List/service/interface.ts
@@ -1,9 +1,19 @@
 export interface GetProductListRequestParams {
   page: number;
   page_size: number;
-  category_id?: string; // 商品分类
-  type?: string; // 商品类型
-  sort_type?: string; // 排序类型
+  category_id?: string; // 商品分类id
+  // 商品类型
+  // 1 - 仅看有货
+  // 2 - 促销商品
+  // 3 - 热卖、打折货
+  type?: string;
+
+  // 排序类型
+  // 1 - 综合、默认排序
+  // 2 - 销量
+  // 3 - 新品
+  sort_type?: string;
+
   max?: string; // 最高价格
   min?: string; // 最低价格
   query?: string; // 搜索条件
